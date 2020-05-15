1a43acd3450dbdac22ef05547af5c4ac2041b682
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-14 19:11:50 -0500

    Configuracion de relaciones

--- app/Like.php
+++ app/Like.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    protected $table = 'Likes';
+
+    /**
+     * Relacion One To Many (invers) / Muchos likes pertenecen a un único usuario
+     * Para obtener el usurio que ha creado el like user_id
+     * 
+     * Indico con que Modelos se relacionar y la propiedad (atributo) que los relaciona
+     */
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    /**
+     * Relacion One To Many (invers) / Muchas likes pertenecen a una img 
+     * Para obtener la img a la pertenece el like image_id
+     * 
+     * Indico con que Modelos se relacionar y la propiedad (atributo) que los relaciona
+     */
+    public function image() {
+        return $this->belongsTo('App\Image', 'image_id');
+    }
 }
