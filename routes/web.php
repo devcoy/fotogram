a17c728ce3be410440316b196afc17e73ae88754
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-14 20:02:37 -0500

    auth de laravel implementado

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 use App\Image;
 
 Route::get('/', function () {
-$images = Image::all();
+/* $images = Image::all();
 foreach($images as $image) {
     //var_dump($images);
     echo $image->image_path . '<br/>';
@@ -26,9 +26,13 @@ foreach($images as $image) {
     }
     echo '<h2>Likes</h2>';
     echo count($image->likes) . '<br/>';
-    echo '<hr/>';
+    echo '<hr/>'; 
 }
-die();
+die(); */
 
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
