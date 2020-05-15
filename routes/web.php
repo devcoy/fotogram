f97cbe13abe74c2c287e9ecface760efcc2cb561
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-14 19:48:15 -0500

    probando el orm

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+use App\Image;
+
 Route::get('/', function () {
+$images = Image::all();
+foreach($images as $image) {
+    //var_dump($images);
+    echo $image->image_path . '<br/>';
+    echo $image->description . '<hr/>';
+    echo $image->user->name . '<br/>';
+    echo '<h2>Comentarios</h2>';
+    foreach($image->comments as $comment) {
+        echo $comment->content . '<br/>';
+    }
+    echo '<h2>Likes</h2>';
+    echo count($image->likes) . '<br/>';
+    echo '<hr/>';
+}
+die();
+
     return view('welcome');
 });
