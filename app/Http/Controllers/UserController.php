86bffac0efdcbc98c413a05babf4a32dce9638cf
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-14 23:08:34 -0500

    subir image de pefil del usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,55 +7,65 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+  //
 
-    /**
-     * Render vista 
-     * 
-     * @return view
-     */
-    public function config() {
-        return view('user.config');
-    }
+  /**
+   * Render vista 
+   * 
+   * @return view
+   */
+  public function config()
+  {
+    return view('user.config');
+  }
 
-    /**
-     * Actualiza los datos del usuario
-     * 
-     * @param Request $request Datos del formulario
-     * 
-     * @return redirect Array con un mensaje
-     */
-    public function update(Request $request) {
+  /**
+   * Actualiza los datos del usuario
+   * 
+   * @param Request $request Datos del formulario
+   * 
+   * @return redirect Array con un mensaje
+   */
+  public function update(Request $request)
+  {
 
-        //Obtener el usuario logeado
-        $user = \Auth::user(); 
-        
-        //Validar el formulario 
-        $validate = $this->validate($request, array(
-            'name' => 'required|string|max:255',
-            'surname' => 'required|string|max:255',
-            //comprueba que el email es único, con la excepción de que cuando sea el del mismo usuario logeado sea valido
-            'nick' => 'required|string|max:255|unique:users,nick, ' . $user->id,
-            'email' => 'required|email|max:255|unique:users,email, ' . $user->id,
-        ));
+    //Obtener el usuario logeado
+    $user = \Auth::user();
 
-        //Obetener los datos que llegan desde el formulario        
-        $name = $request->input('name');
-        $surname = $request->input('surname');
-        $nick = $request->input('nick');
-        $email = $request->input('email');  
-        
-        // Setear los nuevos valores al obj user
-        $user->name = $name;
-        $user->surname = $surname;
-        $user->nick = $nick;
-        $user->email = $email;
+    //Validar el formulario 
+    $validate = $this->validate($request, array(
+      'name' => 'required|string|max:255',
+      'surname' => 'required|string|max:255',
+      //comprueba que el email es único, con la excepción de que cuando sea el del mismo usuario logeado sea valido
+      'nick' => 'required|string|max:255|unique:users,nick, ' . $user->id,
+      'email' => 'required|email|max:255|unique:users,email, ' . $user->id,
+    ));
 
-        //Ejecutar query 
-        $user->update();
+    //Obetener los datos que llegan desde el formulario        
+    $name = $request->input('name');
+    $surname = $request->input('surname');
+    $nick = $request->input('nick');
+    $email = $request->input('email');
 
-        return redirect()->route('config')->with(array(
-            'message' => 'Usuario actualizado correctamente'
-        ));        
-    }
+    // Setear los nuevos valores al obj user
+    $user->name = $name;
+    $user->surname = $surname;
+    $user->nick = $nick;
+    $user->email = $email;
+
+    //Subir img de perfil
+    $image_path = $request->file('image_path');    
+    if ($image_path) {
+      $image_name = time() . $image_path->getClientOriginalName();
+      \Storage::disk('users')->put($image_name, \File::get($image_path));      
+      $user->image = $image_name;
+    } 
+
+    //Ejecutar query 
+    $user->update();
+
+    return redirect()->route('config')->with(array(
+      'message' => 'Usuario actualizado correctamente'
+    ));
+  }
 }
--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="card-body">
-          <form method="POST" action="{{ route('user.update')}}">
+          <form method="POST" action="{{ route('user.update')}}" enctype="multipart/form-data">
             @csrf
 
             <div class="form-group row">
@@ -67,6 +67,20 @@
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
 
                 @error('email')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label for="image_path" class="col-md-4 col-form-label text-md-right">{{ __('Imagen de perfil') }}</label>
+
+              <div class="col-md-6">
+                <input id="image_path" type="file" class=" @error('image_path') is-invalid @enderror" name="image_path" required autocomplete="image_path">
+
+                @error('image_path')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
                 </span>
