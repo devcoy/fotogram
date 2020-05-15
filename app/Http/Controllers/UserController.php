1092d07dc71cc6447600e0b2fb6c436e403288b0
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-15 00:02:08 -0500

    se aplico un middleware a nivel de UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-  //
+  
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
 
   /**
    * Render vista 
