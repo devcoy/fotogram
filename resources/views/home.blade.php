a005f819fa9039795e7935fec41d7f29b0b06f91
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-18 19:27:23 -0500

    mostrar nombre. img y nick de usaurio en el listado de fotos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
 
-            @include ('includes.message');
-            
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+      @include ('includes.message')
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+      @foreach($images as $image)
+      <div class="card">
+        <div class="card-header">
+          @if($image->user->image)
+          <img src="{{ route('user.image', array('filename' => $image->user->image)) }}" alt="{{ $image->user->nick }}" class="image-card__img-profile">
+          @endif
+          <strong>{{ $image->user->name . ' ' . $image->user->surname }}</strong><br />
+          <small>@ {{ $image->user->nick}}</small>
         </div>
+
+        <div class="card-body">
+          @if (session('status'))
+          <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+          </div>
+          @endif
+
+          You are logged in!
+        </div>
+      </div>
+      @endforeach
     </div>
+  </div>
 </div>
 @endsection
