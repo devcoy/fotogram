768b678da3c2fa57ac519b41c016a5504b6b676a
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-31 03:06:14 -0500

    se modifico el texto de Logout a cerrar sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,15 +75,15 @@
               </a>
 
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
-                </a>
                 <a class="dropdown-item" href="{{ route('user.profile', array('id' => Auth::user()->id)) }}"" onclick="" ;>
                   Mi perfil
                 </a>
-                <a class="dropdown-item" href="{{ route('config') }}" onclick="" ;>
+                <a class=" dropdown-item" href="{{ route('config') }}" onclick="" ;>
                   Configuración
+                </a>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                  {{ __('Cerrar sesión') }}
                 </a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                   @csrf
