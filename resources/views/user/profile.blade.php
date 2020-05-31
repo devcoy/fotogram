c5c379bb626a48c7d81ef4528d2536d4c7621880
Jorge Cervantes <jorgecdev@gmail.com>
2020-05-31 01:33:05 -0500

    se agrego la info del usuario. imagen, nombre, apellido y numero de publicaciones

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
 <div class="container">
-  <h1>Publicaciones de {{ $user->name . $user->surname }}</h1>
+  <div class="data-user row justify-content-left align-items-center mt-3 mb-5">
+    <div class="img-profile col-4 col-md-3">
+      @if($user->image)
+      <img src="{{ route('user.image', array('filename' => $user->image)) }}" alt="{{ $user->nick }}" class="">
+      @endif
+    </div>
+    <div class="col-8 col-md-9">
+      <h2> {{ '@' . $user->nick }}</h2>
+      <h4>{{ $user->name . ' ' . $user->surname}}</h4>
+      <small>{{'Miembro desde ' .  \FormatTime::LongTimeFilter($user->created_at) }}</small><br/>
+      <small>{{ count($user->images) . ' Publicaciones' }}</small>
+    </div>
+
+
+  </div>
+
   @include ('includes.message')
-  <div class="row justify-content-left">    
+  <div class="row justify-content-left">
     @if($user->images && count($user->images) > 0)
     @foreach($user->images as $image)
     <div class="col-md-6">
